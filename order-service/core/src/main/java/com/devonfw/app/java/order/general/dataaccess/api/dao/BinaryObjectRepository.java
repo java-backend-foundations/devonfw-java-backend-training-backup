9509608f83bc0c59d300aa51c38f93a7f432e068
Krzysztof Sobkowiak <krzysztof.sobkowiak@capgemini.com>
2019-03-12 09:00:55 +0100

    New Devonfw application generated

---
+++ order-service/core/src/main/java/com/devonfw/app/java/order/general/dataaccess/api/dao/BinaryObjectRepository.java
@@ -0,0 +1,11 @@
+package com.devonfw.app.java.order.general.dataaccess.api.dao;
+
+import com.devonfw.module.jpa.dataaccess.api.data.DefaultRepository;
+import com.devonfw.app.java.order.general.dataaccess.api.BinaryObjectEntity;
+
+/**
+ * {@link DefaultRepository} for {@link BinaryObjectEntity}.
+ */
+public interface BinaryObjectRepository extends DefaultRepository<BinaryObjectEntity> {
+
+}
